8b50ee2cc44635147d09d95472176c0ab2602f06
Faraytodi Atmajaya <179775310+maybeatmajaya@users.noreply.github.com>
2025-05-06 13:45:02 +0700

    connect with firebase

---
+++ lib/models/player.dart
@@ -0,0 +1,7 @@
+class Player {
+  // ignore: prefer_typing_uninitialized_variables
+  var id, name, position, imageUrl, createdAt;
+
+
+  Player({this.position, this.id, this.imageUrl, this.name, this.createdAt});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../controllers/task_controller.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../pages/detail_player_page.dart';
+import '../pages/add_player_page.dart';
+
+import '../providers/players.dart';
 
 class HomePage extends StatelessWidget {
-  final TaskController taskController = Get.put(TaskController());
-
   @override
   Widget build(BuildContext context) {
+    final allPlayerProvider = Provider.of<Players>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('To-Do List')),
-      body: Obx(() => ListView.builder(
-        itemCount: taskController.tasks.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(taskController.tasks[index]),
-          trailing: IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () => taskController.removeTask(index),
+      appBar: AppBar(
+        title: Text("ALL PLAYERS"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.pushNamed(context, AddPlayer.routeName);
+            },
           ),
-        ),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.toNamed('/add'),
-        child: Icon(Icons.add),
+        ],
       ),
+      body: (allPlayerProvider.jumlahPlayer == 0)
+          ? Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "No Data",
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(height: 20),
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, AddPlayer.routeName);
+                    },
+                    child: Text(
+                      "Add Player",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemCount: allPlayerProvider.jumlahPlayer,
+              itemBuilder: (context, index) {
+                var id = allPlayerProvider.allPlayer[index].id;
+                return ListTile(
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      DetailPlayer.routeName,
+                      arguments: id,
+                    );
+                  },
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      allPlayerProvider.allPlayer[index].imageUrl,
+                    ),
+                  ),
+                  title: Text(
+                    allPlayerProvider.allPlayer[index].name,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMMd()
+                        .format(allPlayerProvider.allPlayer[index].createdAt),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      allPlayerProvider.deletePlayer(id, context);
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
